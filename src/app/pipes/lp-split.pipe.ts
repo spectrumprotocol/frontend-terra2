464575bea599bdf99df436b447512f8968e3b86b
SpectrumRhealward <SpectrumRhealward@users.noreply.github.com>
2022-09-18 20:58:07 +0700

    refactor

--- src/app/pipes/lp-split.pipe.ts
+++ src/app/pipes/lp-split.pipe.ts
@@ -1,20 +1,9 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import BigNumber from 'bignumber.js';
 import {CONFIG} from '../consts/config';
-import {AssetInfo, PoolResponse} from '../services/api/terraswap_pair/pool_response';
+import {PoolResponse} from '../services/api/terraswap_pair/pool_response';
 import {UnitPipe} from './unit.pipe';
-import { InfoService } from '../services/info.service';
-
-interface LpSplitData {
-  baseSymbol: string;
-  denomSymbol: string;
-  baseDecimals: number;
-  baseUnit: number;
-  baseAssetInfo: AssetInfo;
-  denomDecimals: number;
-  denomUnit: number;
-  denomAssetInfo: AssetInfo;
-}
+import {Vault} from '../pages/vault/vault.component';
 
 @Pipe({
   name: 'lpSplit'
@@ -23,11 +12,10 @@ export class LpSplitPipe implements PipeTransform {
 
   constructor(
     private unitPipe: UnitPipe,
-    private info: InfoService,
   ) {
   }
 
-  transform(lp: number, poolResponse: PoolResponse, data: LpSplitData, digitsInfo?: string): string {
+  transform(lp: number, poolResponse: PoolResponse, vault: Vault, digitsInfo?: string): string {
     if (typeof lp !== 'number' || !poolResponse) {
       return undefined;
     }
@@ -41,11 +29,11 @@ export class LpSplitPipe implements PipeTransform {
       .div(poolResponse.total_share)
       .toString();
     const asset0Token = poolResponse.assets[0].info.token?.['contract_addr'] || poolResponse.assets[0].info.native_token?.['denom'];
-    const baseToken = data.baseAssetInfo.token?.['contract_addr'] || data.baseAssetInfo.native_token?.['denom'];
+    const baseToken = vault.baseAssetInfo?.['token']?.['contract_addr'] || vault.baseAssetInfo?.['native_token']?.['denom'];
     if (asset0Token === baseToken) {
-      return `${this.unitPipe.transform(amount1, data.baseDecimals, digitsInfo)} ${data.baseSymbol} + ${this.unitPipe.transform(amount2, data.denomDecimals, digitsInfo)} ${data.denomSymbol}`;
+      return `${this.unitPipe.transform(amount1, vault.baseDecimals, digitsInfo)} ${vault.baseSymbol} + ${this.unitPipe.transform(amount2, vault.denomDecimals, digitsInfo)} ${vault.denomSymbol}`;
     } else {
-      return `${this.unitPipe.transform(amount2, data.baseDecimals, digitsInfo)} ${data.baseSymbol} + ${this.unitPipe.transform(amount1, data.denomDecimals, digitsInfo)} ${data.denomSymbol}`;
+      return `${this.unitPipe.transform(amount2, vault.baseDecimals, digitsInfo)} ${vault.baseSymbol} + ${this.unitPipe.transform(amount1, vault.denomDecimals, digitsInfo)} ${vault.denomSymbol}`;
     }
   }
 
