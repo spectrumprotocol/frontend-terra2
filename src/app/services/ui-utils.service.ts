ae56d6be24ad4faf7ca273ea8ca648e426619e99
SpectrumRhealward <SpectrumRhealward@users.noreply.github.com>
2022-09-28 17:22:18 +0700

    Revert "refactor"
    This reverts commit 3df32b4f0e917bc759ceb9463c0699bb5850e460.

--- src/app/services/ui-utils.service.ts
+++ src/app/services/ui-utils.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {PercentPipe} from '@angular/common';
+import {PercentSuperscriptPipe} from '../pipes/percent-superscript.pipe';
 
 @Injectable({
   providedIn: 'root'
@@ -7,37 +7,13 @@ import {PercentPipe} from '@angular/common';
 export class UiUtilsService {
 
   constructor(
-      private percentPipe: PercentPipe
+      private percentSuperscriptPipe: PercentSuperscriptPipe
   ) {
 
   }
 
-  transformPercentSuperscript(value: number, decimals?: number, digitsInfo?: string, locale?: string){
-    function extractE(a: string){
-      const indexOfE = a.indexOf('e');
-      return a.substring(indexOfE);
-    }
-
-    if (value == null) {
-      return value;
-    }
-    if (value > 1e7){
-      try {
-        const valueString = value.toExponential(2).toString();
-        const leftPart = valueString.substring(0, 4);
-        const rightPart = valueString.substring(4);
-        const extractedEPart = extractE(rightPart);
-        // const signOfE = extractedEPart.substring(1, 2);
-        const eValue = +extractedEPart.substring(2);
-        const powerBy = eValue + 2;
-        const rightPartSuperscript = `10<sup>${powerBy}</sup>` ;
-        return ` ${leftPart}x${rightPartSuperscript}%`;
-      } catch (e) {
-        console.error('percentsuperscript', e, value);
-        return ` ` + this.percentPipe.transform(value, digitsInfo, locale);
-      }
-    }
-    return ` ` + this.percentPipe.transform(value, digitsInfo, locale);
+  transformPercentSuperscript(value: number){
+    return this.percentSuperscriptPipe.transform(value);
   }
 
 }
