504a64f1d2941fd4c4b776f3f55acb5c5e35fd49
Spectrum Rhealward <84844988+SpectrumRhealward@users.noreply.github.com>
2023-04-25 23:02:53 +0700

    adjust highlight

--- src/app/services/farm_info/astroport/mainnet_only/astroport-roar-luna-farm-info.service.ts
+++ src/app/services/farm_info/astroport/mainnet_only/astroport-roar-luna-farm-info.service.ts
@@ -29,7 +29,7 @@ export class AstroportRoarLunaFarmInfoService implements FarmInfoService {
   readonly dex: DEX = 'Astroport';
   baseTokenContract: string;
   denomTokenContract: string;
-  readonly highlight = false;
+  readonly highlight = true;
   readonly notUseAstroportGqlApr = false;
   poolAprs: PoolAPR[];
   farmContract: string;
